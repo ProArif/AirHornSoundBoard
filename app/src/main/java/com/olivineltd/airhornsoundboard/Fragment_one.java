b9054ed70a85da1fe063dde08189e7895ec5460e
MahbubHasan1994 <mahabubhasan18@gmail.com>
2020-02-12 15:11:20 +0600

    Test commit

--- app/src/main/java/com/olivineltd/airhornsoundboard/Fragment_one.java
+++ app/src/main/java/com/olivineltd/airhornsoundboard/Fragment_one.java
@@ -34,14 +34,7 @@ public class Fragment_one extends Fragment {
     GifView gifView;
     ImageView backImage;
     String number;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -61,8 +54,7 @@ public class Fragment_one extends Fragment {
     public static Fragment_one newInstance(String param1, String param2) {
         Fragment_one fragment = new Fragment_one();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -71,8 +63,7 @@ public class Fragment_one extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
